27aef217a12384b47b28a34096437d6d933bf4f9
Nyi Nyi Soe <jackemrick2k@gmail.com>
2024-07-04 00:48:53 +0700

    added layout for notes/added floating action button

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,32 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:notify/screens/editnotepage.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          expandedHeight: MediaQuery.of(context).size.height * 0.3,
-          centerTitle: true,
-          pinned: true,
-          flexibleSpace: FlexibleSpaceBar(
-            centerTitle: true,
-            title: const Text('N O T I F Y'),
-            background: Container(
-              color: Colors.greenAccent,
-            ),
+    return SafeArea(
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const EditNotePage();
+                }),
+              );
+            },
+            child: const Icon(Icons.note_add),
           ),
-        ),
-        SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-          return ListTile(
-            title: Text('Item $index'),
-          );
-        }, childCount: 20))
-      ],
-    ));
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                expandedHeight: MediaQuery.of(context).size.height * 0.25,
+                centerTitle: true,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: const Text('N O T I F Y'),
+                  background: Container(
+                    color: Colors.greenAccent,
+                  ),
+                ),
+              ),
+              SliverGrid.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 1.5,
+                    crossAxisSpacing: 10,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Container(
+                      color: Colors.primaries[index % Colors.primaries.length],
+                    ),
+                  );
+                },
+                itemCount: 10,
+              )
+            ],
+          )),
+    );
   }
 }
