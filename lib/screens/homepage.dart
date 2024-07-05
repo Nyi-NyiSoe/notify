364c0bed82f480ca066c97a2522e9f51d1b069fc
Nyi Nyi Soe <jackemrick2k@gmail.com>
2024-07-05 17:18:05 +0700

    added drawer/bottom nav bar/allnotes page/category page

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:notify/screens/allnotes.dart';
+import 'package:notify/screens/categorypage.dart';
 import 'package:notify/screens/editnotepage.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+ int selectedIndex = 0;
+  static const List<Widget> _pages = <Widget>[
+    AllNotes(),
+    CategoryPage()
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            
+            onTap: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            items: const [
+               BottomNavigationBarItem(
+                  icon:  Icon(Icons.note_alt_rounded), label: 'All Notes'),
+              
+              BottomNavigationBarItem(
+                  icon:  Icon(Icons.folder), label: 'Category'),
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(color: Colors.greenAccent),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('User name'),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.file_upload),
+                title: const Text('Upload Notes'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.exit_to_app),
+                title: const Text('Logout'),
+                onTap: () {},
+              ),
+            ]),
+          ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
               Navigator.push(
@@ -33,21 +83,10 @@ class Homepage extends StatelessWidget {
                   ),
                 ),
               ),
-              SliverGrid.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 1.5,
-                    crossAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10),
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Container(
-                      color: Colors.primaries[index % Colors.primaries.length],
-                    ),
-                  );
-                },
-                itemCount: 10,
+              SliverFillRemaining(
+                child: _pages.elementAt(selectedIndex),
               )
+            
             ],
           )),
     );
